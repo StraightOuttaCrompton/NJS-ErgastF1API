3d976191860b830c5e92a70ae1eb6c27bb0fdc7b
Jack Crompton <git@straightouttacrompton.dev>
2023-03-12 13:59:20 +0100

    remove unused items

--- src/routes/circuits.ts
+++ src/routes/circuits.ts
@@ -1,8 +1,7 @@
 import { DEFAULT_LIMIT, DEFAULT_OFFSET } from "../consts";
-import { getMySQLConnection } from "../connection";
 import { Request, Response } from "express";
-import path from "path";
 import { circuits, PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 interface Circuit {
@@ -125,84 +124,11 @@ function parseParams(req: Request, res: Response) {
     };
 }
 
-interface CircuitResponse {
-    MRData: {
-        limit: string;
-        offset: string;
-        CircuitTable: {
-            circuitId?: string;
-            driverId?: string;
-            constructorId?: string;
-            grid?: string;
-            result?: string;
-            fastest?: string;
-            status?: string;
-            season?: string;
-            round?: string;
-            Circuits: Circuit[];
-        };
-    };
-}
-
 export async function getCircuits(req: Request, res: Response) {
     const params = parseParams(req, res);
     if (!params) {
         return;
     }
-
-    // const sqlQuery = getSqlQuery({
-    //     offset,
-    //     limit,
-    //     year,
-    //     round,
-    //     constructor,
-    //     circuit,
-    //     driver,
-    //     grid,
-    //     result,
-    //     fastest,
-    //     status,
-    // });
-
-    // const mySQLConnection = getMySQLConnection();
-    // mySQLConnection.query(
-    //     sqlQuery,
-    //     (
-    //         err,
-    //         // TODO: how to type rows?
-    //         rows,
-    //         fields
-    //     ) => {
-    //         if (err) {
-    //             console.log(
-    //                 "Failed to query for " + __filename.slice(__filename.lastIndexOf(path.sep) + 1) + ": " + err
-    //             );
-    //             res.status(400).send({ error: err.sqlMessage, sql: err.sql }).end();
-    //             return;
-    //         }
-
-    //         if (returnSqlQuery) {
-    //             res.status(200).send(sqlQuery).end();
-    //             return;
-    //         }
-
-    //         const response = formatResponse(rows, {
-    //             offset,
-    //             limit,
-    //             year,
-    //             round,
-    //             constructor,
-    //             circuit,
-    //             driver,
-    //             grid,
-    //             result,
-    //             fastest,
-    //             status,
-    //         });
-
-    //         res.json(response);
-    //     }
-    // );
 
     // let sqlQuery =
     //     "SELECT DISTINCT circuits.circuitRef, circuits.name, circuits.location, circuits.country, circuits.lat, circuits.lng, circuits.alt, circuits.url FROM circuits";
@@ -271,7 +197,7 @@ export async function getCircuits(req: Request, res: Response) {
                                 driverRef: driver,
                             },
                             statusId: status ? { in: [status] } : undefined,
-                            // positionText: result ? { in: [result] } : undefined,
+                            positionText: result ? { in: [result] } : undefined,
                             // grid: grid ? { in: [grid] } : undefined,
                             // rank: fastest ? { in: [fastest] } : undefined,
                         },
@@ -310,118 +236,4 @@ export async function getCircuit(req: Request, res: Response) {
         return;
     }
     res.json(circuit);
-}
-
-// interface CircuitDB {
-//     circuitRef: string;
-//     url: string;
-//     name: string;
-//     lat: number;
-//     lng: number;
-//     alt: number;
-//     location: string;
-//     country: string;
-// }
-
-function getSqlQuery({
-    offset,
-    limit,
-    year,
-    round,
-    constructor,
-    circuit,
-    driver,
-    grid,
-    result,
-    fastest,
-    status,
-}: {
-    offset: number;
-    limit: number;
-    year: string | undefined;
-    round: string | undefined;
-    constructor: string | undefined;
-    circuit: string | undefined;
-    driver: string | undefined;
-    grid: string | undefined;
-    result: string | undefined;
-    fastest: string | undefined;
-    status: string | undefined;
-}) {
-    let sqlQuery =
-        "SELECT DISTINCT circuits.circuitRef, circuits.name, circuits.location, circuits.country, circuits.lat, circuits.lng, circuits.alt, circuits.url FROM circuits";
-    if (year || driver || constructor || status || grid || fastest || result) sqlQuery += ", races";
-    if (driver || constructor || status || grid || fastest || result) sqlQuery += ", results";
-    if (driver) sqlQuery += ", drivers";
-    if (constructor) sqlQuery += ", constructors";
-    sqlQuery += " WHERE TRUE";
-
-    //Set the join
-    if (year || driver || constructor || status || grid || fastest || result)
-        sqlQuery += " AND races.circuitId=circuits.circuitId";
-    if (circuit) sqlQuery += ` AND circuits.circuitRef='${circuit}'`;
-    if (driver || constructor || status || grid || fastest || result) sqlQuery += " AND results.raceId=races.raceId";
-    if (constructor)
-        sqlQuery += ` AND results.constructorId=constructors.constructorId AND constructors.constructorRef='${constructor}'`;
-    if (driver) sqlQuery += ` AND results.driverId=drivers.driverId AND drivers.driverRef='${driver}'`;
-    if (status) sqlQuery += ` AND results.statusId='${status}'`;
-    if (grid) sqlQuery += ` AND results.grid='${grid}'`;
-    if (fastest) sqlQuery += ` AND results.rank='${fastest}'`;
-    if (result) sqlQuery += ` AND results.positionText='${result}'`;
-    if (year) sqlQuery += ` AND races.year='${year}'`;
-    if (round) sqlQuery += ` AND races.round='${round}'`;
-    sqlQuery += ` ORDER BY circuits.circuitRef LIMIT ${offset}, ${limit}`;
-
-    return sqlQuery;
-}
-
-function formatResponse(
-    rows: circuits[],
-    {
-        offset,
-        limit,
-        year,
-        round,
-        constructor,
-        circuit,
-        driver,
-        grid,
-        result,
-        fastest,
-        status,
-    }: {
-        offset: number;
-        limit: number;
-        year: string | undefined;
-        round: string | undefined;
-        constructor: string | undefined;
-        circuit: string | undefined;
-        driver: string | undefined;
-        grid: string | undefined;
-        result: string | undefined;
-        fastest: string | undefined;
-        status: string | undefined;
-    }
-) {
-    const json: CircuitResponse = {
-        MRData: {
-            limit: limit.toString(),
-            offset: offset.toString(),
-            CircuitTable: {
-                Circuits: formatCircuits(rows),
-            },
-        },
-    };
-
-    if (year) json.MRData.CircuitTable.season = year;
-    if (round) json.MRData.CircuitTable.round = round;
-    if (constructor) json.MRData.CircuitTable.constructorId = constructor;
-    if (circuit) json.MRData.CircuitTable.circuitId = circuit;
-    if (driver) json.MRData.CircuitTable.driverId = driver;
-    if (grid) json.MRData.CircuitTable.grid = grid;
-    if (result) json.MRData.CircuitTable.result = result;
-    if (fastest) json.MRData.CircuitTable.fastest = fastest;
-    if (status) json.MRData.CircuitTable.status = status;
-
-    return json;
 }
